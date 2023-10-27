a5f3d2892a88b12860f3701fbe39c555895c7c0e
kalabinii <kalabin_i@bk.ru>
2023-10-27 10:15:04 +0400

    добовлен бутстрап, добовлены гриды

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,14 +5,28 @@ import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contacts';
 import Baranina from '../pages/Baranina_dich';
-import Bobovie from '../pages/Bobovie'
+import Bobovie from '../pages/Bobovie';
+import { Col, Form, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 class Header extends Component {
     render() {
         return (
             <div>
-                <a href='/'>Home</a>
-                <a href='/about'>About</a>
+                <Navbar bg="dark" data-bs-theme="dark">
+                    <Container>
+                    <Navbar.Brand href="/">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                    </Nav>
+                    </Container>
+                </Navbar>
+                
+               
                 <Router>
                     <Routes>
                         <Route path="/" element={<Home />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
+import { Col, Form, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h2>Таблица калорийности продуктов</h2>
-                <Table striped bordered hover>
-                    <tbody>
-                        <tr>
-                            <td><a href='/baranina'>Баранина и дичь</a></td><td><a href='/bobovie'>Бобовые</a></td>
-                        </tr>
-                    </tbody>
-                </Table>
-                
-            </div>
+            <Container>
+                <Row>
+                    <Col sm={4}>
+                        <h4>Таблица калорийности</h4>
+                        <ListGroup>
+                            <ListGroup.Item>dsfdf</ListGroup.Item>
+                            <ListGroup.Item>dsfdf</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col sm={8}>
+                    <h2>Таблица калорийности продуктов</h2>
+                        <Row>
+                            <Col sm={6}><a href='/baranina'>Баранина и дичь</a></Col>
+                            <Col sm={6}><a href='/bobovie'>Бобовые</a></Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+            
         );
     }
 }
